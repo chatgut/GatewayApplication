80c1b9a8127b818bbb6635565545371391cb6612
Lucia Predescu <luciana-579@hotmail.com>
2024-05-15 10:16:05 +0200

    Added custom filter. rebuild of image and modify the compose file. Still not connecting

--- src/main/java/org/example/gatewayapplication/GatewayApplication.java
+++ src/main/java/org/example/gatewayapplication/GatewayApplication.java
@@ -2,45 +2,54 @@ package org.example.gatewayapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 
 @SpringBootApplication
-public class GatewayApplication {
+public class GatewayApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+
     @Bean
-    RouteLocator gateway(RouteLocatorBuilder rlb) {
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder rlb, JwtAuthenticationFilter jwtAuthenticationFilter) {
         return rlb
                 .routes()
                 .route(p -> p
                         .path("/auth/login/**")
-                        .filters(f -> f
-                                .tokenRelay()
-                                .rewritePath("/auth/login/(?<segment>.*)", "/api/auth/login/$\\{segment}"))
-                        .uri("http://localhost:8003"))
+                        .filters(f -> f.tokenRelay()
+                                .rewritePath("/auth/login/(?<segment>.*)", "/api/auth/login/${segment}"))
+                        .uri("http://authservice2:8080"))
                 .route(p -> p
                         .path("/posts/**")
-                        .filters(f -> f.rewritePath("/posts/(?<segment>.*)", "/posts/$\\{segment}"))
-                        .uri("http://localhost:8000"))
+                        .filters(f -> f.filter(jwtAuthenticationFilter)
+                                .rewritePath("/posts/(?<segment>.*)", "/posts/${segment}"))
+                        .uri("http://micropostservices:8000"))
                 .route(p -> p
                         .path("/images/**")
-                        .filters(f -> f.rewritePath("/images/(?<segment>.*)", "/images/$\\{segment}"))
-                        .uri("http://localhost:8001"))
+                        .filters(f -> f.filter(jwtAuthenticationFilter)
+                                .rewritePath("/images/(?<segment>.*)", "/images/${segment}"))
+                        .uri("http://imageservice:8001"))
                 .route(p -> p
                         .path("/users/**")
-                        .filters(f -> f.rewritePath("/users/(?<segment>.*)", "/users/$\\{segment}"))
-                        .uri("http://localhost:8002"))
-
+                        .filters(f -> f.filter(jwtAuthenticationFilter)
+                                .rewritePath("/users/(?<segment>.*)", "/users/${segment}"))
+                        .uri("http://userservice:8002"))
                 .route(p -> p
                         .path("/like/**")
-                        .filters(f -> f.rewritePath("/like/(?<segment>.*)", "/like/$\\{segment}"))
-                        .uri("http://localhost:8005"))
+                        .filters(f -> f.filter(jwtAuthenticationFilter)
+                                .rewritePath("/like/(?<segment>.*)", "/like/${segment}"))
+                        .uri("http://worthreadingservice:8005"))
+                .route(p -> p
+                        .path("/shorten/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter)
+                                .rewritePath("/shorten/(?<segment>.*)", "/shorten/${segment}"))
+                        .uri("http://micro-url-shortener-service:8080"))
                 .build();
     }
 
---
+++ src/main/java/org/example/gatewayapplication/JwtAuthenticationFilter.java
@@ -0,0 +1,78 @@
+package org.example.gatewayapplication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class JwtAuthenticationFilter implements GatewayFilter {
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        // endpoint without login
+        if (request.getURI().getPath().contains("/auth/login")) {
+            return chain.filter(exchange);
+        }
+
+        // get token from authservice2
+        String authHeader = request.getHeaders().getFirst("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatusCode.valueOf(401));
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+
+        // Call the authservice to validate token
+        return webClientBuilder.build()
+                .post()
+                .uri("http://authservice2:8080/api/auth/validate")
+                .header("Authorization", "Bearer " + token)
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, response -> Mono.error(new RuntimeException("Invalid Token")))
+                .bodyToMono(Void.class)
+                .then(chain.filter(exchange))
+                .onErrorResume(e -> {
+                    exchange.getResponse().setStatusCode(HttpStatusCode.valueOf(401));
+                    return exchange.getResponse().setComplete();
+                });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
